8254e6cfa446e247be04063887ee8aa1b3fbaf92
Jubayer Isfar <isfarjubayer@gmail.com>
2026-03-13 00:15:49 +0600

    Remove sensitive files and sanitize API keys

--- Reference/php_files/test_version.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Simple version check file
-echo json_encode([
-    'version' => 'updated_2024_v2',
-    'file' => 'send_otp.php',
-    'timestamp' => date('Y-m-d H:i:s'),
-    'logging_enabled' => true
-]);
-?>
